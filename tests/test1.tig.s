253da68777e62e679e8d92d344c5a6ce4afb344d
Qingyang Chen <qingyang.chen@gmail.com>
2017-04-15 21:28:32 -0400

    uhh

--- tests/test1.tig.s
+++ tests/test1.tig.s
@@ -7,9 +7,11 @@ tigermain:
 	pushl %ebx							# push callee save
 	pushl %edi							# push callee save
 	pushl %esi							# push callee save
-L2:
+L24:
 	movl %ebp, %ebx							# move to register
-	addl $~404, %ebx							# add two registers
+	movl $-404, %edi							# move constant to register
+	addl %edi, %ebx							# add two registers
+	movl %ebx, %ebx							# move to register
 	pushl %eax							# save caller save
 	pushl %ecx							# save caller save
 	pushl %edx							# save caller save
@@ -24,13 +26,34 @@ L2:
 	movl %ebx, %ebx							# move to register
 	movl %ebx, (%ebx)							# move to memory
 	movl %ebp, %ebx							# move to register
-	movl $~404, %edi							# move constant to register
+	movl $-404, %edi							# move constant to register
 	addl %edi, %ebx							# add two registers
 	movl (%ebx), %ebx							# fetch from memory
 	pushl %eax							# save caller save
 	pushl %ecx							# save caller save
 	pushl %edx							# save caller save
-	pushl $L6							# push onto stack
+	pushl $0							# push onto stack
+	call chr
+	addl $4, %esp							# pop arguments
+	movl %eax, %ebx							# get return value
+	popl %edx							# restore caller save
+	popl %ecx							# restore caller save
+	popl %eax							# restore caller save
+	movl %ebx, %ebx							# move to register
+	pushl %eax							# save caller save
+	pushl %ecx							# save caller save
+	pushl %edx							# save caller save
+	pushl %ebx							# push onto stack
+	call print
+	addl $4, %esp							# pop arguments
+	movl %eax, %ebx							# get return value
+	popl %edx							# restore caller save
+	popl %ecx							# restore caller save
+	popl %eax							# restore caller save
+	pushl %eax							# save caller save
+	pushl %ecx							# save caller save
+	pushl %edx							# save caller save
+	pushl $L22							# push onto stack
 	call print
 	addl $4, %esp							# pop arguments
 	movl %eax, %ebx							# get return value
@@ -38,14 +61,17 @@ L2:
 	popl %ecx							# restore caller save
 	popl %eax							# restore caller save
 	movl $1, %eax							# move to register
-	jmp L7							# jump to L7
-L7:
+	jmp L23							# jump to L23
+L23:
 	popl %esi							# pop callee save
 	popl %edi							# pop callee save
 	popl %ebx							# pop callee save
 	movl %ebp, %esp							# deallocate frame
 	popl %ebp							# restore base pointer
 	ret
-L6:
-	.long 4
-	.byte 98, 111, 98, 10, 0
+L22:
+	.long 1
+	.byte 10, 0
+L21:
+	.long 1
+	.byte 48, 0
